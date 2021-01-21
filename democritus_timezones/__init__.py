05492740f25f2c9945bf85b74282dbcd46a39a91
Floyd Hightower <floyd.hightower27@gmail.com>
2021-01-20 20:43:44 -0500

    Updating library with a better version collection system

--- democritus_timezones/__init__.py
+++ democritus_timezones/__init__.py
@@ -1,5 +1,16 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version('democritus_timezones')
+except PackageNotFoundError:
+    # package is not installed
+    message = 'Unable to find a version number for "democritus_timezones"; This is likely indicates the installation of this library failed. Please try re-installing it.'
+    print(message)
+
 __author__ = '''Floyd Hightower'''
 __email__ = 'floyd.hightower27@gmail.com'
-__version__ = '2021.01.08'
 
 from .democritus_timezones import *
--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@
 
 from setuptools import setup, find_packages
 
-from democritus_timezones import __version__
-
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
@@ -15,7 +13,6 @@ with open('requirements_dev.txt') as requirements_dev_file:
 
 setup(
     name='democritus_timezones',
-    version=__version__,
     description="Democritus functions for working with timezones.",
     long_description=readme,
     long_description_content_type="text/markdown",
