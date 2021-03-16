829b810ccbbfedb3e8be4cbff3a1f8847a820e99
Floyd Hightower <floyd.hightower27@gmail.com>
2021-03-16 07:10:57 -0500

    Updating imports to reference new, d8s_... names

--- d8s_timezones/__init__.py
+++ d8s_timezones/__init__.py
@@ -6,10 +6,10 @@ except ImportError:
 from .democritus_timezones import *
 
 try:
-    __version__ = version('democritus_timezones')
+    __version__ = version('d8s_timezones')
 except PackageNotFoundError:
     message = (
-        'Unable to find a version number for "democritus_timezones".'
+        'Unable to find a version number for "d8s_timezones".'
         + 'This likely means the library was not installed properly.'
         + 'Please try re-installing it.'
     )
