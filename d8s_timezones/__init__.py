e7d64a845d515ad4733c8a7f5631a47cb21a1dd4
Floyd Hightower <floyd.hightower27@gmail.com>
2021-03-17 08:09:47 -0500

    Adding support for bumpversion
        Fixes https://github.com/democritus-project/roadmap/issues/69

--- d8s_timezones/__init__.py
+++ d8s_timezones/__init__.py
@@ -1,19 +1,5 @@
-try:
-    from importlib.metadata import PackageNotFoundError, version
-except ImportError:
-    from importlib_metadata import PackageNotFoundError, version  # type: ignore
-
 from .democritus_timezones import *
 
-try:
-    __version__ = version('d8s_timezones')
-except PackageNotFoundError:
-    message = (
-        'Unable to find a version number for "d8s_timezones".'
-        + 'This likely means the library was not installed properly.'
-        + 'Please try re-installing it.'
-    )
-    print(message)
-
+__version__ = '0.2.0'
 __author__ = '''Floyd Hightower'''
 __email__ = 'floyd.hightower27@gmail.com'
